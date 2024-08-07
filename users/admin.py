bcb990385ebf4c13238e43569fde5392370e92f8
meoyoug <eodyd426@gmail.com>
2024-08-07 11:26:32 +0900

    User Admin 코드 활성화

--- users/admin.py
+++ users/admin.py
@@ -4,23 +4,23 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-# @admin.register(User)
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'nickname', 'email', 'is_active', 'is_staff', 'is_superuser']
-#     list_display_links = ['id', 'nickname', 'email']
-#     search_fields = ['nickname', 'email']
-#     readonly_fields = ['id', 'is_staff', 'is_superuser']
-#
-#     fieldsets = (
-#         ('User Info', {'fields': ('id', 'nickname', 'email')}),
-#         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-#     )
-#
-#     add_fieldsets = (
-#         ('Required User Info', {
-#             'classes': ('wide',),
-#             'fields': ('nickname', 'email', 'password1', 'password2'),
-#         }),
-#     )
-#
-#     ordering = ('id',)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nickname', 'email', 'is_active', 'is_staff', 'is_superuser']
+    list_display_links = ['id', 'nickname', 'email']
+    search_fields = ['nickname', 'email']
+    readonly_fields = ['id', 'is_staff', 'is_superuser']
+
+    fieldsets = (
+        ('User Info', {'fields': ('id', 'nickname', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+    add_fieldsets = (
+        ('Required User Info', {
+            'classes': ('wide',),
+            'fields': ('nickname', 'email', 'password1', 'password2'),
+        }),
+    )
+
+    ordering = ('id',)
