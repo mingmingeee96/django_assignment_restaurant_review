6a1b572ceb7e52cabe0e04cacc9cb7d107482b86
meoyoug <eodyd426@gmail.com>
2024-08-07 11:24:19 +0900

    User 시리얼라이저 추가, 모델 테스트 코드 추가, media 폴더 추가 및 기본 프로필 이미지 추가

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'nickname', 'email', 'is_active', 'is_staff', 'is_superuser']
+#     list_display_links = ['id', 'nickname', 'email']
+#     search_fields = ['nickname', 'email']
+#     readonly_fields = ['id', 'is_staff', 'is_superuser']
+#
+#     fieldsets = (
+#         ('User Info', {'fields': ('id', 'nickname', 'email')}),
+#         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+#     )
+#
+#     add_fieldsets = (
+#         ('Required User Info', {
+#             'classes': ('wide',),
+#             'fields': ('nickname', 'email', 'password1', 'password2'),
+#         }),
+#     )
+#
+#     ordering = ('id',)
