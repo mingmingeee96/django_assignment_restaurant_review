16572eeb07757fe4d25ee78803ff0859263953b6
meoyoug <eodyd426@gmail.com>
2024-08-08 17:48:13 +0900

    urlpatterns 추가, URL Include

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from restaurants.urls import router as restaurants_router
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(restaurants_router.urls))
+    path('', include(restaurants_router.urls)),
+    path('users/', include('users.urls')),
 ]
